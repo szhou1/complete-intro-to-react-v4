25d65967448367bc91eba23bf6b5f5ec329efd1d
Steve Zhou <zhousteve3@gmail.com>
2019-03-30 13:05:55 -0700

    Add Redux, remove Context

--- src/App.js
+++ src/App.js
@@ -1,105 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Router } from "@reach/router";
-import pf from "petfinder-client";
-import Loadable from 'react-loadable';
-import { Provider } from "./SearchContext";
+import Loadable from "react-loadable";
 import NavBar from "./NavBar";
-
-const petfinder = pf({
-  key: process.env.API_KEY,
-  secret: process.env.API_SECRET
-});
+import { Provider as ReduxProvider } from "react-redux";
+import store from "./store";
 
 const LoadableDetails = Loadable({
-  loader: () => import('./Details'),
+  loader: () => import("./Details"),
   loading() {
-    return <h1>loading split out code...</h1>
+    return <h1>loading split out code...</h1>;
   }
 });
 
 const LoadableResults = Loadable({
-  loader: () => import('./Results'),
+  loader: () => import("./Results"),
   loading() {
-    return <h1>loading split out results...</h1>
+    return <h1>loading split out results...</h1>;
   }
 });
 
 const LoadableSearchParams = Loadable({
-  loader: () => import('./SearchParams'),
+  loader: () => import("./SearchParams"),
   loading() {
-    return <h1>loading split out SearchParams...</h1>
+    return <h1>loading split out SearchParams...</h1>;
   }
 });
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      location: "Seattle, WA",
-      animal: "",
-      breed: "",
-      breeds: [],
-      handleAnimalChange: this.handleAnimalChange,
-      handleBreedChange: this.handleBreedChange,
-      handleLocationChange: this.handleLocationChange,
-      getBreeds: this.getBreeds
-    };
-  }
-  handleLocationChange = event => {
-    this.setState({
-      location: event.target.value
-    });
-  };
-  handleAnimalChange = event => {
-    this.setState(
-      {
-        animal: event.target.value
-      },
-      this.getBreeds
-    );
-  };
-  handleBreedChange = event => {
-    this.setState({
-      breed: event.target.value
-    });
-  };
-  getBreeds() {
-    if (this.state.animal) {
-      petfinder.breed
-        .list({ animal: this.state.animal })
-        .then(data => {
-          if (
-            data.petfinder &&
-            data.petfinder.breeds &&
-            Array.isArray(data.petfinder.breeds.breed)
-          ) {
-            this.setState({
-              breeds: data.petfinder.breeds.breed
-            });
-          } else {
-            this.setState({ breeds: [] });
-          }
-        })
-        .catch(console.error);
-    } else {
-      this.setState({
-        breeds: []
-      });
-    }
-  }
   render() {
     return (
       <div>
         <NavBar />
-        <Provider value={this.state}>
+        <ReduxProvider store={store}>
           <Router>
             <LoadableResults path="/" />
             <LoadableDetails path="/details/:id" />
             <LoadableSearchParams path="/search-params" />
           </Router>
-        </Provider>
+        </ReduxProvider>
       </div>
     );
   }
